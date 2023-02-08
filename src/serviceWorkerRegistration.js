0982610ec2311992bd93951f7c5ee34d18baa4b5
NelsonDJCR <dev@nelsondjcr.com>
2023-02-07 23:40:41 -0500

    update

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -11,11 +11,12 @@
 // opt-in, read https://cra.link/PWA
 
 const isLocalhost = Boolean(
-  window.location.hostname === 'https://gilded-hummingbird-97b735.netlify.app' ||
+  window.location.hostname === 'https://gilded-hummingbird-97b735.netlify.app' 
+  // ||
     // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
+    // window.location.hostname === '[::1]' ||
     // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+    // window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
 
 export function register(config) {
